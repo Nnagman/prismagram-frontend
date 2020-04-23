78f7be457dca51027367fc705afe941715a7d162
Nangman <ckdgh930314@naver.com>
2020-04-23 11:30:33 +0900

    #8.4 SquarePost Component

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -4,18 +4,26 @@ import PropTypes from "prop-types";
 import FatText from "../../Components/FatText";
 import Loader from "../../Components/Loader";
 import UserCard from "../../Components/UserCard";
+import SquarePost from "../../Components/SquarePost";
 
 const Wrapper = styled.div`
-  height: 50vh;
+  height: 100%;
+  min-height: 50vh;
 `;
 
 const Section = styled.div`
   margin-bottom: 50px;
   display: grid;
   grid-gap: 25px;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(4, 160px);
   grid-template-rows: 160px;
   grid-auto-rows: 160px;
+`;
+
+const PostSection = styled(Section)`
+  grid-template-columns: repeat(4, 200px);
+  grid-template-rows: 200px;
+  grid-auto-rows: 200px;
 `;
 
 const SearchPresenter = ({ searchTerm, loading, data }) => {
@@ -31,7 +39,6 @@ const SearchPresenter = ({ searchTerm, loading, data }) => {
         <Loader />
       </Wrapper>
     );
-    //User를 찾거나 Post를 검색하는 경우를 나눠서 출력
   } else if (data && data.searchUser && data.searchPost) {
     return (
       <Wrapper>
@@ -51,13 +58,19 @@ const SearchPresenter = ({ searchTerm, loading, data }) => {
             ))
           )}
         </Section>
-        <Section>
+        <PostSection>
           {data.searchPost.length === 0 ? (
             <FatText text="No Posts Found" />
           ) : (
-            data.searchPost.map((post) => null)
+            data.searchPost.map((post) => (
+              <SquarePost
+                likeCount={post.likeCount}
+                commentCount={post.commentCount}
+                file={post.files[0]}
+              />
+            ))
           )}
-        </Section>
+        </PostSection>
       </Wrapper>
     );
   }
